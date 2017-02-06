e83fae03ac9b3843d6b51d5f231f78c2aa9d04c6
DictumMortuum <theio.vrefos@gmail.com>
2017-02-06 23:11:32 +0200

    rework

---
+++ community.js
@@ -0,0 +1,125 @@
+/* global Mustache Chartist Tabletop*/
+Tabletop.init({
+  key: '1nzL1ZkFidReY5Xwyue_QONSLwh52HOGLiowAGgkDBc8',
+  simplesheet: true,
+  callback: s => {
+    // Initial data transformations from google docs
+    const sheet = s.plays.elements.map(d => {
+      d.rounds = Number(d.rounds);
+      d.points = Number(d.points);
+      return d;
+    });
+
+    $.getJSON('./scythe.json', scythe => {
+      const countries = ['Rusviet', 'Polania', 'Crimea', 'Nordic', 'Saxony'];
+      const boards = ['Industrial', 'Engineering', 'Patriotic', 'Mechanical', 'Agricultural'];
+      const r = sheet.filter(d => d.rounds).map(d => d.rounds).sort();
+      // .filter((el, i, a) => i === a.indexOf(el));
+      // If the rounds object has "holes", then chartist has a bug!
+      // It doesn't render all the objects!
+      const rounds = Array(r.length).fill(r[0]).map((d, i) => d + i);
+
+      function stackedBar(series, labels, data) {
+        data.col = data.col || 'col2';
+        $('.grid').append(Mustache.render(scythe.template, data));
+
+        new Chartist.Bar('#chart' + data.id + ' > div', {
+          labels: labels,
+          series: series
+        }, {
+          stackBars: true,
+
+          axisY: {
+            labelInterpolationFnc: function (value) {
+              return Number.isInteger(value) ? value : null;
+            }
+          }
+        }).on('draw', function (data) {
+          if (data.type === 'bar') {
+            data.element.attr({
+              style: 'stroke-width: 0px'
+            });
+
+            for (let s = 0; s < data.series.length; s++) {
+              if (data.seriesIndex === s) {
+                data.element.animate({
+                  y2: {
+                    begin: s * 500,
+                    dur: 500,
+                    from: data.y1,
+                    to: data.y2,
+                    easing: Chartist.Svg.Easing.easeOutSine
+                  },
+                  'stroke-width': {
+                    begin: s * 500,
+                    dur: 1,
+                    from: 0,
+                    to: 20,
+                    fill: 'freeze'
+                  }
+                }, false);
+              }
+            }
+          }
+        });
+      }
+
+      function averageWinPoints(acc, cur, p) {
+        if (cur[1] > 0) {
+          acc[p][p] = cur[0] / cur[1];
+        }
+        return acc;
+      }
+
+      function winsByCountry(acc, cur) {
+        let c = countries.indexOf(cur.country);
+        acc[c][c]++;
+        return acc;
+      }
+
+      function winsByBoard(acc, cur) {
+        let b = boards.indexOf(cur.board);
+        let c = countries.indexOf(cur.country);
+        acc[c][b]++;
+        return acc;
+      }
+
+      function winsByRounds(acc, cur) {
+        let r = rounds.indexOf(cur.rounds);
+        let c = countries.indexOf(cur.country);
+        acc[c][r]++;
+        return acc;
+      }
+
+      function winStatistics(acc, cur) {
+        let c = countries.indexOf(cur.country);
+        acc[c][0] += cur.points;
+        acc[c][1]++;
+        return acc;
+      }
+
+      stackedBar(
+        sheet.reduce(winsByCountry, countries.map(() => countries.map(() => 0))),
+        countries, { id: 1, desc: 'Wins' }
+      );
+
+      stackedBar(
+        sheet.reduce(winsByBoard, countries.map(() => boards.map(() => 0))),
+        boards, { id: 2, desc: 'Wins by board' }
+      );
+
+      stackedBar(
+        sheet.filter(d => d.points)
+        .reduce(winStatistics, countries.map(() => [0, 0]))
+        .reduce(averageWinPoints, countries.map(() => countries.map(() => 0))),
+        countries, { id: 3, desc: 'Average winning points' }
+      );
+
+      stackedBar(
+        sheet.filter(d => d.rounds)
+        .reduce(winsByRounds, countries.map(() => rounds.map(() => 0))),
+        rounds, { id: 4, desc: 'Wins by rounds' }
+      );
+    });
+  }
+});
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* global Mustache Chartist*/
 $.getJSON('scythe.json', scythe => {
   const countries = ['Saxony', 'Rusviet', 'Nordic', 'Polania', 'Crimea'];
   const players = ['Dimitris', 'Panagiotis', 'Elena', 'Kostas', 'Kalliopi'];
@@ -5,7 +6,7 @@ $.getJSON('scythe.json', scythe => {
 
   function stackedBar(series, labels, data) {
     data.col = data.col || 'col2';
-    $('.grid').append(Mustache.render(scythe.template, data))
+    $('.grid').append(Mustache.render(scythe.template, data));
 
     new Chartist.Bar('#chart' + data.id + ' > div', {
       labels: labels,
@@ -14,7 +15,7 @@ $.getJSON('scythe.json', scythe => {
       stackBars: true,
 
       axisY: {
-        labelInterpolationFnc: function(value) {
+        labelInterpolationFnc: function (value) {
           return Number.isInteger(value) ? value : null;
         }
       }
@@ -24,22 +25,22 @@ $.getJSON('scythe.json', scythe => {
           style: 'stroke-width: 0px'
         });
 
-        for (var s = 0; s < data.series.length; ++s) {
+        for (let s = 0; s < data.series.length; ++s) {
           if (data.seriesIndex === s) {
             data.element.animate({
               y2: {
-                begin:  s * 500,
-                dur:    500,
-                from:   data.y1,
-                to:     data.y2,
+                begin: s * 500,
+                dur: 500,
+                from: data.y1,
+                to: data.y2,
                 easing: Chartist.Svg.Easing.easeOutSine
               },
               'stroke-width': {
                 begin: s * 500,
-                dur:   1,
-                from:  0,
-                to:    20,
-                fill:  'freeze'
+                dur: 1,
+                from: 0,
+                to: 20,
+                fill: 'freeze'
               }
             }, false);
           }
@@ -49,27 +50,27 @@ $.getJSON('scythe.json', scythe => {
   }
 
   function winsByCountry(acc, cur) {
-    var c = countries.indexOf(cur.country);
-    var p = players.indexOf(cur.winner);
+    let c = countries.indexOf(cur.country);
+    let p = players.indexOf(cur.winner);
     acc[p][c]++;
     return acc;
   }
 
   function winsByBoard(acc, cur) {
-    var b = boards.indexOf(cur.board);
-    var p = players.indexOf(cur.winner);
+    let b = boards.indexOf(cur.board);
+    let p = players.indexOf(cur.winner);
     acc[p][b]++;
     return acc;
   }
 
   function winsByPlayer(acc, cur) {
-    var p = players.indexOf(cur.winner);
+    let p = players.indexOf(cur.winner);
     acc[p][p]++;
     return acc;
   }
 
   function winStatistics(acc, cur) {
-    var p = players.indexOf(cur.winner);
+    let p = players.indexOf(cur.winner);
     acc[p][0] += cur.points;
     acc[p][1]++;
     return acc;
@@ -86,46 +87,47 @@ $.getJSON('scythe.json', scythe => {
     const combinations = countries.filter(d => d === country).map(
       (c) => boards.map((b) => c + ' ' + b)
     ).reduce(
-      (acc, cur) => { cur.forEach((a) => acc.push(a)); return acc; }
-    )
+      (acc, cur) => {
+        cur.forEach((a) => acc.push(a)); return acc;
+      }
+    );
 
     function winsByCombinations(acc, cur) {
-      var p = players.indexOf(cur.winner);
-      var c = combinations.indexOf(cur.country + ' ' + cur.board);
+      let p = players.indexOf(cur.winner);
+      let c = combinations.indexOf(cur.country + ' ' + cur.board);
       acc[p][c]++;
       return acc;
     }
 
     stackedBar(
       scythe.plays.reduce(winsByCombinations, players.map(() => combinations.map(() => 0))),
-      combinations,
-      { id: id, desc: 'Wins by ' +  country + ' combination' }
-    )
+      combinations, { id: id, desc: 'Wins by ' + country + ' combination' }
+    );
   }
 
   stackedBar(
     scythe.plays.reduce(winsByPlayer, players.map(() => players.map(() => 0))),
     players, { id: 1, desc: 'Wins' }
-  )
+  );
 
   stackedBar(
     scythe.plays.filter(d => d.points !== undefined)
     .reduce(winStatistics, players.map(() => [0, 0]))
     .reduce(averageWinPoints, players.map(() => players.map(() => 0))),
     players, { id: 2, desc: 'Average winning points' }
-  )
+  );
 
   stackedBar(
     scythe.plays.reduce(winsByCountry, players.map(() => countries.map(() => 0))),
     countries, { id: 3, desc: 'Wins by country' }
-  )
+  );
 
   stackedBar(
     scythe.plays.reduce(winsByBoard, players.map(() => boards.map(() => 0))),
     boards, { id: 4, desc: 'Wins by board' }
-  )
+  );
 
   countries.forEach(
     (country, index) => combinationOnCountry(5 + index, country)
-  )
-})
+  );
+});
