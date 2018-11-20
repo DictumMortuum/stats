6644dd34df007775eb52735c6909bab80146cfb5
DictumMortuum <theio.vrefos@gmail.com>
2018-11-20 15:28:01 +0200

    Updates

---
+++ precache-manifest.701fc18ff7a9dcb2d5cebbcd2fc2788e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f171c6ebb69b77880a6b",
+    "url": "/scythe/static/js/runtime~main.f171c6eb.js"
+  },
+  {
+    "revision": "2507e3224654832ed09b",
+    "url": "/scythe/static/js/main.2507e322.chunk.js"
+  },
+  {
+    "revision": "c48675bf513bc01221aa",
+    "url": "/scythe/static/js/1.c48675bf.chunk.js"
+  },
+  {
+    "revision": "2507e3224654832ed09b",
+    "url": "/scythe/static/css/main.9f4fe0be.chunk.css"
+  },
+  {
+    "revision": "29128a3889c4fa12e509d12bd5001696",
+    "url": "/scythe/index.html"
+  }
+];
