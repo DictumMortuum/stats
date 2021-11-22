ce1af3c5d69413d91f7f3509bdf17af78000fc8f
DictumMortuum <theio.vrefos@gmail.com>
2021-11-22 14:52:30 +0200

    Updates

---
+++ precache-manifest.d361d9a610910f0aeb73dd64acc62c5f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7523cf1c7a7321d37424",
+    "url": "/static/css/main.d9dfa1b4.chunk.css"
+  },
+  {
+    "revision": "7523cf1c7a7321d37424",
+    "url": "/static/js/main.c52d3ea8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d56c72ac95bb40cc96f8",
+    "url": "/static/js/2.b67f427b.chunk.js"
+  },
+  {
+    "revision": "5f8946f95c9fcabbdb35c2f1bc698f64",
+    "url": "/static/media/img.5f8946f9.jpg"
+  },
+  {
+    "revision": "a340c198a4d04d5781b6dc2e0f45401d",
+    "url": "/static/media/logo.a340c198.jpg"
+  },
+  {
+    "revision": "4f2283897f3987b3c92e05d39d9ecdc5",
+    "url": "/static/media/img.4f228389.jpg"
+  },
+  {
+    "revision": "75906379bd2d074f3f85972c905561b8",
+    "url": "/static/media/logo.75906379.jpg"
+  },
+  {
+    "revision": "b1a21f15ec651e1b7206ffec34557918",
+    "url": "/static/media/img.b1a21f15.jpg"
+  },
+  {
+    "revision": "ddd14f6cd2e4f96ee1f344229c27e342",
+    "url": "/static/media/logo.ddd14f6c.png"
+  },
+  {
+    "revision": "af3367462904a271de458b0cee2941b9",
+    "url": "/static/media/logo.af336746.png"
+  },
+  {
+    "revision": "99bfc6b236de30a99c5a41bd6a766753",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cee7894f27e2e0ccc0e0c28b0fe4e31b.js"
+  "/precache-manifest.d361d9a610910f0aeb73dd64acc62c5f.js"
 );
 
 workbox.clientsClaim();
