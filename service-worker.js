b792be8d8cfa1281004ec7de40419d11ac25f2f0
DictumMortuum <theio.vrefos@gmail.com>
2021-11-22 17:34:18 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34cec8a80278440e72389cb5837bee64.js"
+  "/precache-manifest.903326686cf032b92d4002a2f1aa7cbd.js"
 );
 
 workbox.clientsClaim();
