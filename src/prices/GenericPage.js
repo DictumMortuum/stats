aa29b98ecc90a6f00e521d6521136f05c35ecc99
DictumMortuum <theio.vrefos@gmail.com>
2022-03-16 09:21:04 +0200

    [Prices] added bookmarklet

--- src/prices/GenericPage.js
+++ src/prices/GenericPage.js
@@ -7,16 +7,20 @@ import Badge from '@material-ui/core/Badge';
 import StockDropdown from './StockDropdown';
 import StoreDropdown from './StoreDropdown';
 import Breadcrumbs from './Breadcrumbs';
-import PriceIcon from '@material-ui/icons/LocalOffer';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import IconButton from '@material-ui/core/IconButton';
 import { useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import EmptyImg from './cartoff.svg';
 import SearchInput from './SearchInput';
-import SearchIcon from '@material-ui/icons/Search';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Container from '@material-ui/core/Container';
+import {
+  Bookmark,
+  ShoppingCart,
+  LocalOffer,
+  Search
+} from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -45,6 +49,19 @@ const Nothing = props => (
   </Grid>
 )
 
+// https://caiorss.github.io/bookmarklet-maker/
+// var raw = window.location.toString().split("/")[4]
+// var id = parseInt(raw)
+
+// if (isNaN(id)) {
+//   alert("Could not find a boardgame id. Please navigate to a boardgame page.")
+// } else {
+//   var url = "https://stats.dictummortuum.com/#/prices/item/" + id + "?stock=1"
+//   window.open(url, '_blank')
+// }
+
+/* eslint-disable no-script-url */
+
 export default props => {
   const classes = useStyles();
   const { data : json, cart_show, search_results, spinner } = useSelector(state => state.pricesReducer)
@@ -59,19 +76,22 @@ export default props => {
           <SearchInput />
           <Badge className={classes.margin} badgeContent={search_results.length} color="secondary" max={99999}>
             <Link to={"/prices/search"} style={{ color: "white" }}>
-              <SearchIcon />
+              <Search />
             </Link>
           </Badge>
           <Badge className={classes.margin} badgeContent={json.length} color="secondary" max={99999}>
             <Link to={"/prices/all"} style={{ color: "white" }}>
-              <PriceIcon />
+              <LocalOffer />
             </Link>
           </Badge>
           <Badge badgeContent={cart_show.length} color="secondary" max={99999}>
             <Link to={"/prices/cart"} style={{ color: "white" }}>
-              <ShoppingCartIcon />
+              <ShoppingCart />
             </Link>
           </Badge>
+          <IconButton style={{ color: "white" }} href="javascript:(function()%7Bvar%20raw%20%3D%20window.location.toString().split(%22%2F%22)%5B4%5D%0Avar%20id%20%3D%20parseInt(raw)%0A%0Aif%20(isNaN(id))%20%7B%0A%20%20alert(%22Could%20not%20find%20a%20boardgame%20id.%20Please%20navigate%20to%20a%20boardgame%20page.%22)%0A%7D%20else%20%7B%0A%20%20var%20url%20%3D%20%22https%3A%2F%2Fstats.dictummortuum.com%2F%23%2Fprices%2Fitem%2F%22%20%2B%20id%20%2B%20%22%3Fstock%3D1%22%0A%20%20window.open(url%2C%20'_blank')%20%0A%7D%7D)()%3B">
+            <Bookmark />
+          </IconButton>
         </Toolbar>
       </AppBar>
 
