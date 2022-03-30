23a865b78e48960568f414e1b2950344d637de19
DictumMortuum <theio.vrefos@gmail.com>
2022-03-30 23:27:00 +0300

    [prices] added the Paper surface on the store and stock dropdowns. Closes #11

--- src/prices/StockDropdown.js
+++ src/prices/StockDropdown.js
@@ -4,6 +4,7 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
 import { useParams } from '../common';
 
 export default () => {
@@ -28,21 +29,23 @@ export default () => {
   }, [stocks, url_stock]);
 
   return (
-    <FormControl variant="outlined" fullWidth>
-      <InputLabel id="stock-select-label">Stock</InputLabel>
-      <Select
-        labelId="stock-select-label"
-        id="stock-select"
-        value={stock}
-        onChange={(event) => {
-          dispatch({
-            type: "SET_STOCK",
-            stock: event.target.value
-          })
-        }}
-      >
-        {stocks.map(d => <MenuItem key={d} value={d}>{d}</MenuItem>)}
-      </Select>
-    </FormControl>
+    <Paper>
+      <FormControl variant="outlined" fullWidth>
+        <InputLabel id="stock-select-label">Stock</InputLabel>
+        <Select
+          labelId="stock-select-label"
+          id="stock-select"
+          value={stock}
+          onChange={(event) => {
+            dispatch({
+              type: "SET_STOCK",
+              stock: event.target.value
+            })
+          }}
+        >
+          {stocks.map(d => <MenuItem key={d} value={d}>{d}</MenuItem>)}
+        </Select>
+      </FormControl>
+    </Paper>
   )
 }
--- src/prices/StoreDropdown.js
+++ src/prices/StoreDropdown.js
@@ -4,6 +4,7 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
 
 export default props => {
   const { stores } = props;
@@ -11,22 +12,24 @@ export default props => {
   const dispatch = useDispatch();
 
   return (
-    <FormControl variant="outlined" fullWidth>
-      <InputLabel id="store-select-label">Store</InputLabel>
-      <Select
-        labelId="store-select-label"
-        id="store-select"
-        value={store}
-        onChange={(event) => {
-          dispatch({
-            type: "SET_STORE",
-            store: event.target.value
-          })
-        }}
-      >
-        <MenuItem key={-1} value="">None</MenuItem>
-        {stores.map(d => <MenuItem key={d} value={d}>{d}</MenuItem>)}
-      </Select>
-    </FormControl>
+    <Paper>
+      <FormControl variant="outlined" fullWidth>
+        <InputLabel id="store-select-label">Store</InputLabel>
+        <Select
+          labelId="store-select-label"
+          id="store-select"
+          value={store}
+          onChange={(event) => {
+            dispatch({
+              type: "SET_STORE",
+              store: event.target.value
+            })
+          }}
+        >
+          <MenuItem key={-1} value="">None</MenuItem>
+          {stores.map(d => <MenuItem key={d} value={d}>{d}</MenuItem>)}
+        </Select>
+      </FormControl>
+    </Paper>
   )
 }
